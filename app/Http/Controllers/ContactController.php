9cbd89ac5c8d11b4eefe4eddbd62a22775128416
ghazianshari <ghazi.anshari@gmail.com>
2025-12-23 18:37:36 +0700

    creating base model Contact

--- app/Http/Controllers/ContactController.php
+++ app/Http/Controllers/ContactController.php
@@ -5,7 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use Inertia\Inertia;
 
-class ContactsController extends Controller
+class ContactController extends Controller
 {
     public function index()
     {
